7c5f9bc9ee696fb9fd2bd34310ea192b7bc11809
xiaocan817 <xiaocan817@aliyun.com>
2021-06-19 13:01:52 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author XC
+ * @date 2021/6/19 - 12:55
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
